aefc7b2006f23c29e93bec9052643a44e67b89ee
alyannapauline24 <alyannapauline24@github.com>
2015-12-07 04:34:32 +0800

    Fixed Lexemes Classification

--- Bla/MainWindow.cs
+++ Bla/MainWindow.cs
@@ -54,6 +54,139 @@ public partial class MainWindow: Gtk.Window
 	}
 
 	public void addLexemes(string value, string classification) {
+		switch(classification){		//Classification determiner
+		case "HAI":
+		case "KTHXBYE":
+			classification = "Code block delimiter";
+			break;
+		case "BTW":
+		case "OBTW":
+		case "TLDR":
+			classification = "Comment delimiter";
+			break;
+		case "I_HAS_A":
+			classification = "Variable Declaration";
+			break;
+		case "ITZ":
+		case "R":
+			classification = "Variable Assignment";
+			break;
+		case "SUM_OF":
+		case "DIFF_OF":
+		case "PRODUKT_OF":
+		case "QUOSHUNT_OF":
+		case "MOD_OF":
+		case "BIGGR_OF":
+		case "SMALLR_OF":
+			classification = "Math Operator";
+			break;
+		case "BOTH_OF":
+		case "EITHER_OF":
+		case "WON_OF":
+		case "NOT":
+		case "ALL_OF":
+		case "ANY_OF":
+			classification = "Boolean Operator";
+			break;
+		case "BOTH_SAEM":
+			classification = "Equality Operator";
+			break;
+		case "DIFFRINT":
+			classification = "Inequality Operator";
+			break;
+		case "UPPIN":
+			classification = "Increment Operator";
+			break;
+		case "NERFIN":
+			classification = "Decrement Operator";
+			break;
+		case "SMOOSH":
+			classification = "Concatenation Operator";
+			break;
+		case "MAEK":
+		case "IS_NOW_A":
+		case "VISIBLE":
+			classification = "Output Keyword";
+			break;
+		case "GIMMEH":
+			classification = "Input Keyword";
+			break;
+		case "O_RLY":
+			classification = "If-Then Delimiter";
+			break;
+		case "YA_RLY":
+			classification = "If Keyword";
+			break;
+		case "MEBBE":
+			classification = "Else-If Keyword";
+			break;
+		case "NO_WAI":
+			classification = "Else Keyword";
+			break;
+		case "OIC":
+			classification = "If-Then/Switch Delimiter";
+			break;
+		case "WTF":
+			classification = "Switch Delimiter";
+			break;
+		case "OMG":
+			classification = "Case Keyword";
+			break;
+		case "OMGWTF":
+			classification = "Default Keyword";
+			break;
+		case "GTFO":
+			classification = "Break Keyword";
+			break;
+		case "STRING_DELIMETER":
+			classification = "String Delimiter";
+			break;
+		case "A":
+		case "AN":
+			classification = "Argument Separator";
+			break;
+		case "YR":
+			classification = "Parameter Keyword";
+			break;
+		case "MKAY":
+			classification = "Arity Delimiter";
+			break;
+		case "VARIABLE_IDENTIFIER":
+			classification = "Variable Identifier";
+			break;
+		case "NUMBR_LITERAL":
+		case "NUMBAR_LITERAL":
+		case "YARN_LITERAL":
+		case "TROOF_LITERAL":
+		case "TYPE_LITERAL":
+			classification = "Literal";
+			break;
+		case "STATEMENT_DELIMETER":
+			classification = "Statement Delimiter";
+			break;
+		case "HOW_IZ_I":
+		case "IF_U_SAY_SO":
+			classification = "Function Delimiter";
+			break;
+		case "I_IZ":
+			classification = "Function Call Keyword";
+			break;
+		case "FOUND_YR":
+			classification = "Return Keyword";
+			break;
+		case "IM_IN_YR":
+		case "IM_OUTTA_YR":
+			classification = "Loop Delimiter";
+			break;
+		case "TIL":
+		case "WILE":
+			classification = "Condition Keyword";
+			break;
+		case "EXCLAMATION":
+			classification = "New Line Remover";
+			break;
+		}
+
 		lexemeStore.AppendValues (value, classification);
 	}
 
